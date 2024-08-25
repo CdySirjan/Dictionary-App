d8e92329b26f597952bc66692d01411fa85c4a70
CdySirjan <yoursirjan@gmail.com>
2024-08-25 08:55:15 -1200

    Dictionary half codes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [word, setWord] = useState('');
+    const [result, setResult] = useState('');
+
+    const handleSearch = () => {
+      //fetching the dictionary api here
+
+
+      
+        setResult(`Definition of ${word}`);
+    };
+
+    return (
+        <div className="container">
+            <h1>Dictionary App</h1>
+            <div className="search-box">
+                <input
+                    type="text"
+                    id="inputs"
+                    placeholder="Enter a word"
+                    value={word}
+                    onChange={(e) => setWord(e.target.value)}
+                />
+                <button id="searchBtn" onClick={handleSearch}>
+                    Search
+                </button>
+            </div>
+            <div id="result">
+                {result && (
+                    <div className="definition">
+                        <p>{result}</p>
+                    </div>
+                )}
+            </div>
+        </div>
+    );
 }
 
 export default App;
